7ea6c87e660a75386f1b6b00b78c7648d218f7fe
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-10-23 01:06:28 +0530

    backend modifications

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a7395019056d002353c2185eb823adf296d5ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adec7e6951127913f3b271f5e3597f261913c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
