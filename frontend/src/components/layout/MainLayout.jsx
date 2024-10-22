62df5dafa72e3fc4b8a4494c7d7ec436fd3c2a90
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-10-23 01:30:08 +0530

    frontend - routing added,  tailwind configuration

---
+++ frontend/src/components/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Router from "../../router/Router";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Router />
+    </div>
+  );
+};
+
+export default MainLayout;
